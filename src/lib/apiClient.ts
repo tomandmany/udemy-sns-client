9d49c67c27deacf44820ff22eb9e1fd2e24eff05
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2023-10-26 00:50:03 +0900

    baseurl

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  // baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
